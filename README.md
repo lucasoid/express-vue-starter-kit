5b2c24caeb05dceab6e389494425be2272c4b2d4
Ben Lucas <ben@sustaincreativity.com>
2018-10-19 21:53:36 -0400

    Initial commit - Express scaffold

---
+++ README.md
@@ -0,0 +1,7 @@
+# Express + Vue starter kit
+Sample client application using Express, Vue, and Pure CSS. 
+
+* Simple stateless Express server
+* Vue for components
+* Pure for CSS
+* Jest for component tests
---
+++ server.js
@@ -0,0 +1,57 @@
+/**
+ * very simple stateless Express server for logging users in and out, and serving a single static logged-in page.
+ * looks for a cookie named 'accessToken' to verify the user is logged in.
+ * 
+ */
+require('dotenv').config();
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+
+app.listen(port, () => {
+    console.log('listening on ' + port);
+});
+
+const LOGIN_PATH = '/login';
+const LOGOUT_PATH = '/logout';
+const HOME_PATH = '/';
+const ACCESS_TOKEN_KEY = 'accessToken';
+
+const verifyToken = (req, res, next) => {
+    if(req.cookies[ACCESS_TOKEN_KEY]) {
+        try {
+            verified = jwt.verify(req.cookies[ACCESS_TOKEN_KEY], process.env.JWT_KEY);
+            if(verified) return next();
+        }
+        catch (e) {
+            //
+        }
+    }
+    return res.redirect(LOGIN_PATH); 
+}
+
+app.get(LOGIN_PATH, (req, res, next) => {
+    // present login page
+    // fetch and set token
+    res.status(200).send('login'); 
+});
+
+app.get(LOGOUT_PATH, (req, res, next) => {
+    res.clearCookie(ACCESS_TOKEN_KEY);
+    return res.redirect(LOGIN_PATH);
+})
+app.get(HOME_PATH, verifyToken, (req, res, next) => {
+    // cool we're in, display the app
+    res.status(200).send('hello');    
+})
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal server error');
+});
+
